0ef76042b495ffff5c4c00940750940b8169f1e7
Werner Poeijer, van <wpoe@BOKA26747>
2018-01-29 15:26:01 +0100

    First upload

---
+++ ManageAlerts/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace ManageAlerts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string webUrl = "https://boskalis.sharepoint.com/sites/eagle/community/";
+            string userName = "wpoeb@boskalis.com";
+            Console.WriteLine("Please input your password: ");
+            SecureString password = CreateSecureStringPasswordFromConsoleInput();
+            using (var context = new ClientContext(webUrl))
+            {
+                
+                context.Credentials = new SharePointOnlineCredentials(userName, password);
+                context.Load(context.Web, w => w.Title);
+                context.ExecuteQuery();
+
+                GetAlerts(context);
+            }
+        }
+
+        private static void GetAlerts(ClientContext context)
+        {
+            Web web = context.Web;
+            
+            context.Load(web);
+            context.Load(web.Alerts);
+            context.ExecuteQuery();
+
+            Console.WriteLine("Get alerts from web: " + web.Title);
+
+            AlertCollection alerts = web.Alerts;
+
+            foreach (Alert alert in alerts)
+            {
+                web.Alerts.DeleteAlert(alert.ID);
+            }
+            web.Update();
+            Console.WriteLine("Update Alerts..");
+            context.ExecuteQuery();
+
+            Console.ReadKey(true);
+        }
+
+        private static SecureString CreateSecureStringPasswordFromConsoleInput()
+        {
+            ConsoleKeyInfo info;
+
+            //Get the user's password as a SecureString
+            SecureString securePassword = new SecureString();
+            do
+            {
+                info = Console.ReadKey(true);
+                if (info.Key != ConsoleKey.Enter)
+                {
+                    securePassword.AppendChar(info.KeyChar);
+                }
+            }
+            while (info.Key != ConsoleKey.Enter);
+            return securePassword;
+        }
+    }
+}
---
+++ ManageAlerts/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ManageAlerts")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Royal Boskalis Westminster nv")]
+[assembly: AssemblyProduct("ManageAlerts")]
+[assembly: AssemblyCopyright("Copyright © Royal Boskalis Westminster nv 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("81543e1f-d2b3-47f6-bc14-ba46d8500e35")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
